9d25f02e1e12c1324e82ddc619a9032a1c831c8d
shameetha <jashamispark@gmail.com>
2016-06-06 14:00:54 +0530

    Create nmbr.c

---
+++ nmbr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int nmbr;
+  scanf("%d",&nmbr);
+    if(nmbr>0)
+    {
+       printf("POSITIVE");
+    }
+    else
+    {
+      printf("NEGATIVE");
+    }
+}
