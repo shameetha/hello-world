dfd6e75958559e48e4da85de50371b88b8175e14
shameetha <jashamispark@gmail.com>
2016-06-06 14:04:31 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int nmbr;
+  scanf("%d",&nmbr);
+  if(nmbr%2==0)
+  {
+      printf("even");
+  }
+   else
+   {
+      printf("odd");
+    }
+}
